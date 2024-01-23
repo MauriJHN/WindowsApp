ee4e19c3d58cf4d6bc34cee1ee5b7c571fc9ed9a
mhern <mhern@KANGURO>
2024-01-23 19:33:50 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Hello, this is a message from Mars. I'm Elon Musk, and I approve this message";
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Rich text box was changed.");
+        }
+    }
+}
